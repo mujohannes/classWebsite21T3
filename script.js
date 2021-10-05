04923bd0c8976227ee9983177b74cb3910da281f
johannes <johannes.muljana@tafensw.edu.au>
2021-10-05 11:54:49 +1100

    added nav button and javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+function onLoad() {
+  const btn = document.querySelector('.nav-button')
+  const nav = document.querySelector('.navigation')
+
+  btn.addEventListener('click', function() {
+    if( nav.classList.contains('open') ) {
+      nav.classList.remove('open')
+    }
+    else {
+      nav.classList.add('open')
+    }
+  } )
+}
+window.addEventListener('load', onLoad )
