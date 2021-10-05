9c72976cca907d0c77ff48332266edb3d3a170ff
johannes <johannes.muljana@tafensw.edu.au>
2021-10-05 12:29:40 +1100

    update to pages

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function onLoad() {
   const btn = document.querySelector('.nav-button')
   const nav = document.querySelector('.navigation')
 
-  btn.addEventListener('click', function() {
-    if( nav.classList.contains('open') ) {
+  btn.addEventListener('click', function () {
+    if (nav.classList.contains('open')) {
       nav.classList.remove('open')
     }
     else {
       nav.classList.add('open')
     }
-  } )
+  }
+  )
 }
-window.addEventListener('load', onLoad )
+window.addEventListener('load', onLoad)
+
